d794328e5348c6b81aa6bf24c268055d155d9aaf
Daniel Amah <dannyamah@gmail.com>
2024-01-13 11:57:37 +0100

    mobile update

--- app.js
+++ app.js
@@ -27,13 +27,26 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("Download clicked!");
       });
   
-      const jpgIcon = document.createElement("div");
-      jpgIcon.classList.add("icon");
-      jpgIcon.innerHTML = "&#x1F5C4"; // Unicode for file-image icon
-      jpgIcon.addEventListener("click", function () {
-        // Add functionality for jpg action
-        alert("JPG clicked!");
-      });
+    // Get the modal
+      var modal = document.getElementById("myModal");
+
+      // Get the image and insert it inside the modal - use its "alt" text as a caption
+      var img = document.getElementById("myImg");
+      var modalImg = document.getElementById("img01");
+      var captionText = document.getElementById("caption");
+      img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      }
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
   
       overlay.appendChild(downloadIcon);
       overlay.appendChild(jpgIcon);
